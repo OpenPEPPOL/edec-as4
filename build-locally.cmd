de595961f176cf6c3829be2e89101e52874c5632
Philip Helger <philip@helger.com>
2021-04-13 23:23:42 +0200

    Script to build locally on Windows

---
+++ build-locally.cmd
@@ -0,0 +1,4 @@
+@echo off
+docker run --pull --rm -i -v %CD%/docs:/documents -v %CD%/target/site:/target difi/asciidoctor
+docker run --pull --rm -i -v %CD%/target:/src -w /src/site kramos/alpine-zip -r /src/ticc-as4-master.zip .
+::docker run --rm -i -v %CD%:/src alpine:3.8 chown -R $(id -g $USER).$(id -g $USER) /src/target
